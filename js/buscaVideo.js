ae8914d25ea724e8dfe2979487a0c87375728e05
Marcos Cucoro <92962002+MarcosCucoro@users.noreply.github.com>
2023-06-05 17:17:19 -0300

    Add files via upload

---
+++ js/buscaVideo.js
@@ -0,0 +1,33 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+    evento.preventDefault()
+
+    const dadosPesquisa = document.querySelector('[data-pesquisa]').value
+    const busca = await conectaApi.buscaVideo(dadosPesquisa)
+
+    const lista = document.querySelector('[data-lista]')
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(
+            elemento.titulo,
+            elemento.deescricao,
+            elemento.url, 
+            elemento.imagem
+        )
+    ))
+    
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class='mensagem__titulo'>Não existem vídeos com esse termo</h2>`
+    }
+
+}
+
+const botaoPesquisa = document.querySelector('[data-botao-pesquisa]')
+
+botaoPesquisa.addEventListener('click', evento => buscarVideo(evento))
